c0b1ccfee81a4aee00b7e500b98e60bc3c23db10
samarth36483 <71208433+samarth36483@users.noreply.github.com>
2020-12-03 16:40:00 +0530

    Add files via upload

---
+++ src/main/java/com/example/samarth/SamarthApplication.java
@@ -0,0 +1,16 @@
+package com.example.samarth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example.*")
+public class SamarthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamarthApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/example/samarth/SamarthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samarth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamarthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
